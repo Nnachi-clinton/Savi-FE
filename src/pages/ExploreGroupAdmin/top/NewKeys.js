da4b621b53722763a858680196e3e5c906abf221
Nnachi-clinton <clinton.amadi@decagon.dev>
2024-02-12 20:33:00 +0100

    Implemented the admin group details page

--- src/pages/ExploreGroupAdmin/top/NewKeys.js
+++ src/pages/ExploreGroupAdmin/top/NewKeys.js
@@ -2,17 +2,41 @@ import Icon from './Icon.js';
 import './style.css';
 import './Glo.css';
 import Cards from '../card/Cards.js';
+import React, { useState } from 'react';
+import ActiveCards from '../../ExploreGroupAdmin copy/card/ActiveCards.js';
 
 function NewKeys() {
+  const [activeButton, setActiveButton] = useState('All');
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
   return (
     <>
       <div className="savings-groups">
         <p className="savings-group-title">Saving Groups</p>
         <div className="saving-groups-container">
           <div className="flex-container-with-buttons-and-text">
-            <button className="button-all">All</button>
-            <p className="text-container">Active</p>
-            <p className="text-container">New</p>
+            <button
+              className={`button-all ${activeButton === 'All' ? 'active' : ''}`}
+              onClick={() => handleButtonClick('All')}
+            >
+              All
+            </button>
+            <button
+              className={`button-active ${
+                activeButton === 'Active' ? 'active' : ''
+              }`}
+              onClick={() => handleButtonClick('Active')}
+            >
+              Active
+            </button>
+            <button
+              className={`button-new ${activeButton === 'New' ? 'active' : ''}`}
+              onClick={() => handleButtonClick('New')}
+            >
+              New
+            </button>
           </div>
           <button className="button-container">
             <Icon className="svg-container" />
@@ -20,7 +44,10 @@ function NewKeys() {
           </button>
         </div>
       </div>
-      <Cards />
+      {/* <Cards /> */}
+      {activeButton === 'All' && <Cards />}
+      {activeButton === 'Active' && <ActiveCards />}
+      {activeButton === 'New' && <p>I am New Screen</p>}
     </>
   );
 }
